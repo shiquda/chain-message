5cfe26a5c368758937f14d3aa15cb76e91a892c0
shiquda <czy1847259360@gmail.com>
2025-04-21 12:58:28 +0800

    feat: 添加WalletConnect等钱包连接支持

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,35 @@
 import { http, createConfig } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
-import { injected } from 'wagmi/connectors'
+import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import {
+  okxWallet,
+  metaMaskWallet,
+  coinbaseWallet,
+  walletConnectWallet,
+  binanceWallet
+} from '@rainbow-me/rainbowkit/wallets';
+
+const walletConnectProjectId = import.meta.env.VITE_WALLETCONNECT_ID
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Recommended',
+      wallets: [walletConnectWallet, metaMaskWallet, okxWallet, ],
+    },
+    {
+      groupName: 'Others',
+      wallets: [coinbaseWallet, binanceWallet],
+    },
+  ],
+  { appName: "Chain Message", projectId: walletConnectProjectId}
+);
+
 
 export const config = createConfig({
+  connectors,
   chains: [mainnet],
-  connectors: [
-    injected(),
-  ],
   transports: {
-    [mainnet.id]: http(import.meta.env.VITE_PUBLIC_RPC_URL),
+    [mainnet.id]: http(),
   },
 }) 
